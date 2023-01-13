04b6ba9a85c4dd0bc9ae11745e1aeed8681e7d4d
dylandbl <dylandbl.dev@gmail.com>
2023-01-12 20:07:02 -0700

    chore: updated eslint rule configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,13 +15,49 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'jest'],
   rules: {
-    'react/jsx-filename-extension': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-one-expression-per-line': 'off',
+    'arrow-body-style': 'off',
+    'function-paren-newline': 'off',
+    'implicit-arrow-linebreak': 'off', // Conflicts with Prettier.
     'import/extensions': 'off',
+    'import/no-unresolved': 'off', // Handled by TS.
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'linebreak-style': 0, // May want to reconfigure for your project.
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-undef': 'off', // Handled by TS.
+    'no-unused-vars': 'warn',
     'object-curly-newline': 'off',
-    // May want to change for your project.
-    'linebreak-style': 0,
+    'operator-linebreak': 'off', // Conflicts with Prettier.
+    'react/destructuring-assignment': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off', // Does not work with interfaces.
   },
   settings: {
     'import/resolver': {
